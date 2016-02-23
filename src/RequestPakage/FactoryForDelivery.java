5b2577b05c38512b29fe848a815a1ea0416003f8
georgiPavlov <georgi95.bg@gmail.com>
2016-02-23 20:04:18 +0200

    starting work finally :)

--- src/RequestPakage/FactoryForDelivery.java
+++ src/RequestPakage/FactoryForDelivery.java
@@ -1,7 +1,21 @@
 package RequestPakage;
 
+import ProjectInterfaces.DeliveryRequest;
+
 /**
  * Created by georgipavlov on 23.02.16.
  */
 public class FactoryForDelivery {
+
+
+    public DeliveryRequest maker(int choice){
+        switch (choice) {
+            case 1: {
+                return  new Delivery();
+            }case 2: {
+                return new Supply();
+            }
+        }
+        return null;
+    }
 }
--- src/RequestPakage/Request.java
+++ src/RequestPakage/Request.java
@@ -1,6 +1,8 @@
 package RequestPakage;
 
 import DroneDeliveryPakage.Warehouse;
+import ExeptionsPakage.InputExeption;
+import ProjectInterfaces.DeliveryRequest;
 
 import java.util.Scanner;
 
@@ -11,17 +13,42 @@ public class Request {
     private Warehouse currentWarehouse;
     private Scanner scanner;
 
-    public Request(Warehouse currentWarehouse){
-     this.currentWarehouse = currentWarehouse;
+    public Request(Warehouse currentWarehouse) {
+        this.currentWarehouse = currentWarehouse;
         scanner = new Scanner(System.in);
     }
 
-    public void addToWarehouse(){
+    public void addToWarehouse() throws InputExeption {
         FactoryForDelivery factory = new FactoryForDelivery();
+        DeliveryRequest request;
+        System.out.println("Enter operation (1)delivery (2)supply (3)exit");
+        boolean loop = true;
 
-        System.out.println("Enter operation (1)delivery (2)supply");
+        while (loop) {
+            int choice;
+            String tryNumber = scanner.nextLine();
+            if (match(tryNumber)) {
+                choice = Integer.parseInt(tryNumber);
+            } else {
+                throw new InputExeption("in (Enter operation (1)delivery (2)supply)");
+            }
+            if(choice == 3){
+                loop = false;
+                continue;
+            }
+            request = factory.maker(choice);
+            request.startOperation(scanner);
+        }
 
 
+    }
 
+
+    private boolean match(String line) {
+        if (line.equals("1") || line.equals("2") || line.equals("3")) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
