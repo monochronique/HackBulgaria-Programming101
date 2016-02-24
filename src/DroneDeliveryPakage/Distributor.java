9472f859041690f1d246eb3f3145ac45d58e6a1f
georgiPavlov <georgi95.bg@gmail.com>
2016-02-24 04:01:55 +0200

    update 7

--- src/DroneDeliveryPakage/Distributor.java
+++ src/DroneDeliveryPakage/Distributor.java
@@ -1,12 +1,27 @@
 package DroneDeliveryPakage;
 
+import DronePakage.Drone;
 import sun.awt.windows.ThemeReader;
+
+import java.util.ArrayList;
 
 /**
  * Created by georgipavlov on 24.02.16.
  */
 public class Distributor extends Thread {
+    private  final int NUMBER_OF_DRONES = 10;
     private boolean loop = true;
+    private ArrayList<Drone> drones;
+
+    public Distributor(){
+
+    }
+
+    public void startDrones(){
+        for (int i = 0; i <NUMBER_OF_DRONES ; i++) {
+            //drones.add(new Drone())
+        }
+    }
 
     public boolean isLoop() {
         return loop;
